f1e7690aeb8dd18bc6ce06f03da4694d6777451e
dianetables <118228073+dianetables@users.noreply.github.com>
2023-03-07 02:56:01 -0800

    Initial commit

---
+++ src/main/java/net/daine/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.daine.tutorialmod.item;
+
+import net.daine.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static net.minecraft.core.particles.ParticleTypes.ITEM; //idk
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MODID);
+    //create more items by using the same format but change the name
+    //update en_us.json
+    //add respective json files
+    //add image texture
+    public static final RegistryObject<Item> ACMIUM = //change DeferredRegister to RegistryObject
+            ITEMS.register("acmium", () -> new Item(new   //() -> supplier add items in correct order
+                    Item.Properties().tab(ModCreativeModTab.TUTORIAL_TAB)));
+    /*public static final RegistryObject<Item> ACM_BLOCK =
+            ITEMS.register("acm_block", () -> new Item(new
+                    Item.Properties().tab(ModCreativeModTab.TUTORIAL_TAB)));
+*/
+}
